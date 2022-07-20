b120afef401e60ce95ab3b6fab7cd2679970f854
Yuan-Hao Chen <yhchen0906@gmail.com>
2022-07-21 02:50:27 +0800

    update JoyPixels for Ubuntu 20.04
    Signed-off-by: Yuan-Hao Chen <yhchen0906@gmail.com>

--- ubuntu/joypixels.sh
+++ ubuntu/joypixels.sh
@@ -41,6 +41,36 @@ diff -Naur fonts/conf.avail/60-generic.conf fonts-patched/conf.avail/60-generic.
  			<!-- System fonts -->
 EOF
 POST_INSTALL_EOF
+elif [ "$VERSION_ID" = "20.04" ]; then
+  post_install << "POST_INSTALL_EOF"
+sudo patch -d /etc/fonts -p 1 -r - -N << "EOF"
+diff -Naur fonts/conf.avail/45-generic.conf fonts-patched/conf.avail/45-generic.conf
+--- fonts/conf.avail/45-generic.conf	2022-07-20 11:27:31.954131395 -0700
++++ fonts-patched/conf.avail/45-generic.conf	2022-07-20 11:28:12.574376298 -0700
+@@ -34,6 +34,10 @@
+ 	</alias>
+ 	<!-- Third-party emoji -->
+ 	<alias binding="same">
++		<family>JoyPixels</family>
++		<default><family>emoji</family></default>
++	</alias>
++	<alias binding="same">
+ 		<family>Emoji Two</family>
+ 		<default><family>emoji</family></default>
+ 	</alias>
+diff -Naur fonts/conf.avail/60-generic.conf fonts-patched/conf.avail/60-generic.conf
+--- fonts/conf.avail/60-generic.conf	2022-07-20 11:27:31.954131395 -0700
++++ fonts-patched/conf.avail/60-generic.conf	2022-07-20 11:28:39.394447502 -0700
+@@ -41,6 +41,7 @@
+ 			<family>Twitter Color Emoji</family> <!-- Twitter -->
+ 			<family>EmojiOne Mozilla</family> <!-- Mozilla -->
+ 			<!-- Third-Party fonts -->
++			<family>JoyPixels</family>
+ 			<family>Emoji Two</family>
+ 			<family>Emoji One</family>
+ 			<!-- Non-color -->
+EOF
+POST_INSTALL_EOF
 fi
 
 fc_cache
