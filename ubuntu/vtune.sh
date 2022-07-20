400b2904680539531e880e9aa587f612cddfc4a3
Yuan-Hao Chen <yhchen0906@gmail.com>
2022-07-21 02:39:18 +0800

    add current user to vtune group & add desktop entry
    Signed-off-by: Yuan-Hao Chen <yhchen0906@gmail.com>

--- ubuntu/vtune.sh
+++ ubuntu/vtune.sh
@@ -10,4 +10,24 @@ add_apt_list oneAPI 'deb https://apt.repos.intel.com/oneapi all main'
 
 apt_install intel-oneapi-vtune
 
+post_install << "POST_INSTALL_EOF"
+sudo usermod -aG vtune $(whoami)
+
+cat > "$APPS_DIR/vtune-gui.desktop" << EOF
+[Desktop Entry]
+Version=1.0
+Type=Application
+Name=Intel VTune Profiler
+GenericName=VTune
+Comment=Intel® VTune™ Profiler optimizes application performance, system performance, and system configuration for HPC, cloud, IoT, media, storage, and more.
+Exec=bash -c "source /opt/intel/oneapi/vtune/latest/vtune-vars.sh; exec vtune-gui"
+Terminal=false
+MimeType=text/plain;
+Icon=/opt/intel/oneapi/vtune/latest/bin64/resources/app/icons/VTune.png
+Categories=Development;
+StartupNotify=false
+Actions=Window;
+EOF
+POST_INSTALL_EOF
+
 finalize
