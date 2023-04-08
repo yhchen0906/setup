31ca5539da55e9f08b4b31bf7c0f33ca7849e990
Yuan-Hao Chen <yhchen0906@gmail.com>
2023-04-09 02:40:33 +0800

    always show context in p10k
    Signed-off-by: Yuan-Hao Chen <yhchen0906@gmail.com>

--- files/p10k.zsh
+++ files/p10k.zsh
@@ -878,10 +878,6 @@
   # Default context format (no privileges, no SSH): user@hostname.
   typeset -g POWERLEVEL9K_CONTEXT_TEMPLATE='%n@%m'
 
-  # Don't show context unless running with privileges or in SSH.
-  # Tip: Remove the next line to always show context.
-  typeset -g POWERLEVEL9K_CONTEXT_{DEFAULT,SUDO}_{CONTENT,VISUAL_IDENTIFIER}_EXPANSION=
-
   # Custom icon.
   # typeset -g POWERLEVEL9K_CONTEXT_VISUAL_IDENTIFIER_EXPANSION='⭐'
   # Custom prefix.
