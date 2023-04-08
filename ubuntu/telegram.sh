6fe55623240496f53c6c8c554701552112e42f69
Yuan-Hao Chen <yhchen0906@gmail.com>
2023-04-09 00:39:44 +0800

    Create Telegram shortcut with another approach
    Signed-off-by: Yuan-Hao Chen <yhchen0906@gmail.com>

--- ubuntu/telegram.sh
+++ ubuntu/telegram.sh
@@ -19,54 +19,7 @@ post_install << "POST_INSTALL_EOF"
 rm -rf "$OPT_DIR/Telegram"
 tar Jxf "$TMP_DIR/tsetup.tar.xz" -C "$OPT_DIR"
 
-TELEGRAM_BIN="$OPT_DIR/Telegram/Telegram"
-ln -sf "$TELEGRAM_BIN" "$BIN_DIR"
-
-TELEGRAM_PATH_MD5=$(echo -n "$TELEGRAM_BIN" | md5sum | cut -d ' ' -f 1)
-TELEGRAM_DESKTOP_NAME="org.telegram.desktop._${TELEGRAM_PATH_MD5}.desktop"
-
-cat > "$APPS_DIR/$TELEGRAM_DESKTOP_NAME" << "EOF"
-[Desktop Entry]
-Name=Telegram Desktop
-Comment=Official desktop version of Telegram messaging app
-TryExec=Telegram
-Exec=Telegram -- %u
-Icon=telegram
-Terminal=false
-StartupWMClass=TelegramDesktop
-Type=Application
-Categories=Chat;Network;InstantMessaging;Qt;
-MimeType=x-scheme-handler/tg;
-Keywords=tg;chat;im;messaging;messenger;sms;tdesktop;
-Actions=quit;
-SingleMainWindow=true
-X-GNOME-UsesNotifications=true
-X-GNOME-SingleWindow=true
-
-[Desktop Action quit]
-Exec=Telegram -quit
-Name=Quit Telegram
-Icon=application-exit
-EOF
-
-cat > "$AUTOSTART_DIR/$TELEGRAM_DESKTOP_NAME" << "EOF"
-[Desktop Entry]
-Name=Telegram Desktop
-Comment=Official desktop version of Telegram messaging app
-TryExec=Telegram
-Exec=Telegram -autostart
-Icon=telegram
-Terminal=false
-StartupWMClass=TelegramDesktop
-Type=Application
-Categories=Chat;Network;InstantMessaging;Qt;
-MimeType=x-scheme-handler/tg;
-Keywords=tg;chat;im;messaging;messenger;sms;tdesktop;
-Actions=quit;
-SingleMainWindow=true
-X-GNOME-UsesNotifications=true
-X-GNOME-SingleWindow=true
-EOF
+"$OPT_DIR/Telegram/Telegram" -autostart > /dev/null 2>&1
 POST_INSTALL_EOF
 
 finalize
